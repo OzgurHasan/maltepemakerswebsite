1b27fa8156fea95ecb878f09df8d41fab49c0f42
OzgurHasan <ozgrhsnockk@gmail.com>
2018-04-15 17:07:15 +0300

    Yüklemeden sonraki ilk commit

--- MaltepeMakers/index.php
+++ MaltepeMakers/index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/maltepemakers-full-content.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
 <title>Maltepe Makers - Anasayfa</title>
-<link rel="stylesheet" type="text/css" href="style.css" />
+<!-- InstanceEndEditable -->
+<link rel="stylesheet" type="text/css" href="style.css">
 
     <script>
 x = 0;
@@ -22,32 +24,34 @@ $(window).on('scroll', function () {
     }
 });
 	</script>
-
+    <!-- InstanceBeginEditable name="head" -->
+    <!-- InstanceEndEditable -->
 </head>
 <body>
 <div class="navbar">
-<div class="nav"> <a href="#"><img class="navbuton" src="images/MaltepeMakersLogo.png" width="140" height="48" alt="maltepemakersbuton" /></a>
+<div class="nav"> <a href="index.php"><img class="navbuton" src="images/MaltepeMakersLogo.png" width="140" height="48" alt="maltepemakersbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Anasayfa.png" width="88" height="32" alt="anasayfabuton" /></a>
+  <a href="index.php"><img class="navbuton" src="images/Anasayfa.png" width="88" height="32" alt="anasayfabuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Etkinlikler Kayıt.png" width="124" height="32" alt="etkinliklerbuton" /></a>
+  <a href="EtkinliklerKayıt.php"><img class="navbuton" src="images/Etkinlikler%20Kayıt.png" width="124" height="32" alt="etkinliklerbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Projelerimiz.png" width="88" height="32" alt="projelerimizbuton" /></a>
+  <a href="Projelerimiz.php"><img class="navbuton" src="images/Projelerimiz.png" width="88" height="32" alt="projelerimizbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Blog.png" width="62" height="32" alt="blogbuton" /></a>
+  <a href="Blog.php"><img class="navbuton" src="images/Blog.png" width="62" height="32" alt="blogbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Hakkımızda.png" width="80" height="32" alt="hakkımızdabuton" /></a>
+  <a href="Hakkımızda.php"><img class="navbuton" src="images/Hakkımızda.png" width="80" height="32" alt="hakkımızdabuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/İletişim.png" width="63" height="32" alt="iletisimbuton" /></a>
+  <a href="Iletişim.php"><img class="navbuton" src="images/İletişim.png" width="63" height="32" alt="iletisimbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Üye Ol.png" width="60" height="30" alt="uyeolbuton" /></a>
+  <a href="#"><img class="navbuton" src="images/Üye%20Ol.png" width="60" height="30" alt="uyeolbuton"></a>
 
-  <a href="#"><img class="navbuton" src="images/Oturum Aç.png" width="86" height="30" alt="oturumacbuton" /></a>
+  <img class="navbuton" src="images/Oturum%20Aç.png" width="86" height="30" alt="oturumacbuton" onClick="document.getElementById('id01').style.display='block'">
 </div>
 </div>
 
 
 <div class="content">
+<!-- InstanceBeginEditable name="content" -->
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue lobortis erat in aliquam. Mauris blandit erat et urna interdum, non auctor nibh condimentum. Etiam quis augue ultricies, elementum massa nec, feugiat sem. Phasellus eget urna tincidunt, dictum ex sit amet, luctus dolor. Etiam sollicitudin volutpat velit. Donec pellentesque ipsum a magna ultricies, pulvinar convallis nulla dictum. Sed ac feugiat odio.
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et semper nunc, non pharetra justo. Vivamus gravida ultricies lectus, ac convallis risus vehicula id. Suspendisse bibendum elementum arcu, eget cursus sem lobortis et. Aliquam nulla enim, semper et suscipit vitae, ullamcorper vestibulum velit. Sed viverra quis tortor eget bibendum. Quisque ac cursus ligula. Nunc posuere tempor sapien, in ultrices velit eleifend mollis. Etiam faucibus sodales gravida. Vestibulum vel tellus congue, porttitor metus nec, ornare quam. Integer ex ex, cursus sit amet metus eu, fermentum ultrices odio. Vivamus sit amet pretium ex. Phasellus consectetur arcu ac gravida consequat. In condimentum tincidunt dictum.
@@ -248,17 +252,47 @@ Vestibulum ac ex at dolor lacinia ornare. Suspendisse sit amet ornare diam. Ut o
 
 Curabitur sit amet urna lobortis, laoreet lorem vel, egestas lorem. Curabitur a nisi mi. Pellentesque vitae arcu quam. Proin eu quam sed elit interdum aliquet id in elit. Donec felis libero, ultricies at diam lacinia, vehicula venenatis ex. Fusce sodales scelerisque lorem at ultricies. Nam tempor diam nec dapibus finibus. Donec ipsum dolor, suscipit venenatis quam quis, auctor tristique enim. In dui dui, malesuada sit amet porta id, consectetur hendrerit sem. Morbi neque magna, dignissim vel dui sit amet, dignissim mattis ex. Morbi hendrerit bibendum nulla, in consequat massa pharetra eget.
 
-In auctor lacus eget justo feugiat, sit amet condimentum neque convallis. Sed vel ornare lorem, ac interdum lorem. Nam in nunc vitae dolor molestie ornare. Donec egestas interdum erat id posuere. Aliquam quis ligula condimentum, vestibulum sapien in, molestie nulla. Maecenas suscipit mauris lorem, laoreet condimentum elit auctor vel. Phasellus varius feugiat turpis et consectetur. Donec nec sapien fringilla, facilisis diam id, faucibus leo. Ut a facilisis massa. Vestibulum eget sapien eu lacus facilisis vulputate. Morbi malesuada dui id ligula viverra, eget tincidunt mi pretium. Morbi cursus ligula ut erat sagittis, id maximus purus suscipit.
+In auctor lacus eget justo feugiat, sit amet condimentum neque convallis. Sed vel ornare lorem, ac interdum lorem. Nam in nunc vitae dolor molestie ornare. Donec egestas interdum erat id posuere. Aliquam quis ligula condimentum, vestibulum sapien in, molestie nulla. Maecenas suscipit mauris lorem, laoreet condimentum elit auctor vel. Phasellus varius feugiat turpis et consectetur. Donec nec sapien fringilla, facilisis diam id, faucibus leo. Ut a facilisis massa. Vestibulum eget sapien eu lacus facilisis vulputate. Morbi malesuada dui id ligula viverra, eget tincidunt mi pretium. Morbi cursus ligula ut erat sagittis, id maximus purus suscipit.<!-- InstanceEndEditable --></div>
+
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+   
+    </div>
+
+    <div class="container2">
+      <label for="uname"><b>E-Mail Adresi</b></label>
+      <input type="text" placeholder="E-Mail adresinizi girin..." name="uname" required>
+
+      <label for="psw"><b>Şifre</b></label>
+      <input type="password" placeholder="Şifrenizi girin..." name="psw" required>
+        
+      <button type="submit">Giriş Yap</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Beni hatırla
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">İptal</button>
+    </div>
+  </form>
 </div>
+
+
+
 
 <footer class="footer">
     <div class="container" id="sec4">
 
-<a href="https://tr-tr.facebook.com/maltepemakers/"><img class="social" src="images/facebook.png" alt="facebookbuton" width="40" height="40" /></a>
-<a href="https://twitter.com/maltepemaker"><img class="social" src="images/twitter.png" alt="twitterbuton" width="40" height="40" /></a>
-<a href="https://www.instagram.com/maltepemakers/"><img class="social" src="images/instagram.png" alt="instagrambuton" width="40" height="40" /></a>
-<a href="https://www.linkedin.com/maltepemakers"><img class="social" src="images/linkedin.png" alt="linkedInbuton" width="40" height="40" /></a>
-<a href="https://plus.google.com/maltepemakers"><img class="social" src="images/googlep.png" alt="googleplusbuton" width="40" height="40" /></a>
+<a href="https://tr-tr.facebook.com/maltepemakers/"><img class="social" src="images/facebook.png" alt="facebookbuton" width="40" height="40"></a>
+<a href="https://twitter.com/maltepemaker"><img class="social" src="images/twitter.png" alt="twitterbuton" width="40" height="40"></a>
+<a href="https://www.instagram.com/maltepemakers/"><img class="social" src="images/instagram.png" alt="instagrambuton" width="40" height="40"></a>
+<a href="https://www.linkedin.com/maltepemakers"><img class="social" src="images/linkedin.png" alt="linkedInbuton" width="40" height="40"></a>
+<a href="https://plus.google.com/maltepemakers"><img class="social" src="images/googlep.png" alt="googleplusbuton" width="40" height="40"></a>
   </div>
   
   <div class="container">
@@ -266,5 +300,17 @@ In auctor lacus eget justo feugiat, sit amet condimentum neque convallis. Sed ve
   </div>
 </footer>
 
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 </body>
-</html>
+<!-- InstanceEnd --></html>
